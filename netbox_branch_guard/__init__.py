f678b604f434aed81d4b69d76fcb5a0883d361bb
KPTheProf <>
2026-06-10 14:41:57 +0100

    Fix for missing plugin details

--- netbox_branch_guard/__init__.py
+++ netbox_branch_guard/__init__.py
@@ -1,7 +1,43 @@
-from .plugin import config
+from netbox.plugins import PluginConfig
+from django.conf import settings
+from importlib.metadata import version, PackageNotFoundError
 
 try:
     from ._version import __version__
 except ImportError:
     _version__ = "0.0.dev0"
 
+
+class NetboxBranchGuardConfig(PluginConfig):
+    name = "netbox_branch_guard"
+    verbose_name = "Netbox Branch Guard"
+    description = "Guards against writes to the Main branch and enforces branch usage"
+    version = __version__
+    author = "KPTheProf"
+    source = "https://github.com/KPTheProf/netbox_branch_guard"
+    base_url = "netbox-branch-guard"
+
+    min_version = "4.6.0"  # or whatever NetBox version you support
+
+
+
+    def ready(self):
+        middleware_path = "netbox_branch_guard.middleware.NetboxBranchGuardMiddleware"
+
+        if middleware_path in settings.MIDDLEWARE:
+            return
+
+        # Find AuthenticationMiddleware
+        try:
+            index = settings.MIDDLEWARE.index(
+                "django.contrib.messages.middleware.MessageMiddleware"
+            )
+        except ValueError:
+            # fallback: append at end
+            settings.MIDDLEWARE.append(middleware_path)
+            return
+
+        # Insert AFTER auth middleware
+        settings.MIDDLEWARE.insert(index + 1, middleware_path)
+
+config = NetboxBranchGuardConfig
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools_scm import get_version
 
 setup(
     name = "netbox_branch_guard",
+    author = "KPTheProf",
+    url = "https://github.com/KPTheProf/netbox_branch_guard",
+    license = "Apache-2.0",
 
     use_scm_version={
         "write_to": "netbox_branch_guard/_version.py",
