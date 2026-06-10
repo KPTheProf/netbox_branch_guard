e34c9c388940476e8edf8e11cbec24cbdd530a5f
KPTheProf <>
2026-06-10 12:37:49 +0100

    Move the generation of _version.py to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
     name = "netbox_branch_guard",
     description = "Guards against writes to the Main branch and enforces branch usage",
     version=get_version(root=".", relative_to=__file__),
+
+    use_scm_version={
+        "write_to": "netbox_branch_guard/_version.py",
+        "fallback_version": "0.0.dev0",
+    },
+
     author = "KPTheProf",
     url = "https://github.com/KPTheProf/netbox_branch_guard",
     license = "Apache License 2.0",
