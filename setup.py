d162b64cf0fd6303d7d396183ee593064543b32a
KPTheProf <>
2026-06-10 12:45:58 +0100

    removed version field from setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools_scm import get_version
 setup(
     name = "netbox_branch_guard",
     description = "Guards against writes to the Main branch and enforces branch usage",
-    version=get_version(root=".", relative_to=__file__),
 
     use_scm_version={
         "write_to": "netbox_branch_guard/_version.py",
