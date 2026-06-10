c1a489241b6ae5625c3c3f49ac07c03395eda1f7
KPTheProf <>
2026-06-10 12:27:06 +0100

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools_scm import get_version
 setup(
     name = "netbox_branch_guard",
     description = "Guards against writes to the Main branch and enforces branch usage",
-    version=get_version(root=".", relative_to=__file__)
+    version=get_version(root=".", relative_to=__file__),
     author = "KPTheProf",
     url = "https://github.com/KPTheProf/netbox_branch_guard",
     license = "Apache License 2.0",
